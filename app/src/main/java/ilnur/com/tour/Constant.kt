1fdca1b2b63e8447a0282f93ec11752ccf102b23
Ilnur <ulaevilnur@rambler.ru>
2018-11-29 12:48:38 +0500

    change constants

--- app/src/main/java/ilnur/com/tour/Constant.kt
+++ app/src/main/java/ilnur/com/tour/Constant.kt
@@ -3,23 +3,5 @@ package ilnur.com.tour
 class Constant {
     companion object {
         const val API_ROOT = "https://api.myjson.com/"
-
-        val SECURITY_PREFERENCES = "security_preferences"
-        val TOKEN = "token"
-        val CITY = "city"
-
-        const val RESULT_CLOSER_ACTIVITY = 0
-        const val RESULT_AUTHORIZATION = 1
-        const val RESULT_FAVORITES = 2
-        const val RESULT_PROFILE = 3
-        const val RESULT_CITY = 4
-        const val RESULT_CREATE_VEHICLE = 5
-        const val RESULT_ADD_ROUTE = 6
-
-        val PROVIDE_DISTANCE:Float = 0.0f
-        val PROVIDE_TIME:Long = 1000
-        val ACCESS_ON_THE_ROUTE = 1
-        private val MAX_SPEED = 30//(m/s)
-        val DISTANCE_ERROR = MAX_SPEED * PROVIDE_TIME / 1000
     }
 }
